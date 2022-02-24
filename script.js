8739d6678a2589736344d8ad282afdb65a5a1d84
Aura <75125762+Aura974@users.noreply.github.com>
2022-02-24 13:55:41 +0400

    Modal created by JS

--- script.js
+++ script.js
@@ -4,42 +4,6 @@ const allUrls = [
     "http://localhost:8000/api/v1/titles/?genre=fantasy&sort_by=-imdb_score&page_size=7",
     "http://localhost:8000/api/v1/titles/?genre=comedy&sort_by=-imdb_score&page_size=7",
     "http://localhost:8000/api/v1/titles/?genre=drama&sort_by=-imdb_score&page_size=7"];
-
-async function getData(url, movieData) {
-    var movieList = [];
-    response = await fetch(url);
-    resJson = await response.json();
-    for (var i = 0; i < 7; i++) {
-        movieList.push(baseUrl + resJson.results[i].id);
-    }
-    for (var i = 0; i < 7; i++) {
-        resData = await fetch(movieList[i]);
-        resJsonData = await resData.json();
-        movieData.push(resJsonData);
-    }
-    return movieData;
-}
-
-async function bestFilm(url, genre) {
-    var bestFilm = [];
-    await getData(url, bestFilm);
-    genre.querySelector("img").src = bestFilm[0].image_url;
-    genre.getElementsByClassName("info")[0].querySelector("h1.best_film_title").innerText = bestFilm[0].title;
-    genre.getElementsByClassName("info")[0].querySelector("p.description").innerText = bestFilm[0].description;
-}
-
-async function carousel(url, genre, movieData) {
-    await getData(url, movieData);
-    for (var i = 0, j = 0; i < 4; i++, j++) {
-        genre[i].querySelector("img").src = movieData[j].image_url;
-        genre[i].querySelector("img").setAttribute("id",movieData[j].id );
-    }
-    for (var i = 4, j = 3; i < 8; i++, j++) {
-        genre[i].querySelector("img").src = movieData[j].image_url;
-        genre[i].querySelector("img").setAttribute("id",movieData[j].id );
-    }    
-}
-
 const movieGenres = [
     document.getElementById("best_film"),
     document.querySelectorAll("#crsl1_sect1 div.film, #crsl1_sect2 div.film"),
@@ -50,13 +14,113 @@ const movieGenres = [
 var bestRating = [];
 var fantasy = [];
 var comedy = [];
-var drama = [];
+var drama = []; 
 
-bestFilm(allUrls[0], movieGenres[0], bestRating);
-carousel(allUrls[0], movieGenres[1], bestRating);
-carousel(allUrls[1], movieGenres[2], fantasy);
-carousel(allUrls[2], movieGenres[3], comedy);
-carousel(allUrls[3], movieGenres[4], drama);
+async function getMoviesList(url, movieData) {
+    response = await fetch(url);
+    resJson = await response.json();
+    for (var i = 0; i < resJson.results.length; i++) {
+        movieData.push(baseUrl + resJson.results[i].id);
+    }
+}
+
+async function bestFilm(url, genre) {
+    response = await fetch(url);
+    resJson = await response.json();
+
+    genre.querySelector("img").src = resJson.image_url;
+    genre.getElementsByClassName("info")[0].querySelector("h1.best_film_title").innerText = resJson.title;
+    genre.getElementsByClassName("info")[0].querySelector("p.description").innerText = resJson.description;
+
+    modal = createModal();
+    aside = document.createElement("aside");
+    aside.appendChild(modal);
+    genre.appendChild(aside);
+    genre.querySelector("aside div").setAttribute("id", resJson.id);
+
+    genre.querySelector("#open-modal").dataset.open = resJson.id;
+
+    modalData(resJson);
+
+
+    
+
+}
+
+async function carousel(urlList, genreList) {
+    let jsonList = [];
+
+    for (var i = 0; i < urlList.length; i++){
+        response = await fetch(urlList[i]);
+        resJson = await response.json();
+        jsonList.push(resJson);
+    }
+    
+    for (var i = 0; i < 4; i++) {
+        genreList[i].querySelector("img").src = jsonList[i].image_url;
+        genreList[i].querySelector("img").innerHTML = modal; 
+    }
+
+    for (var i = 4, j = 3; i < 8; i++, j++) {
+        genreList[i].querySelector("img").src = jsonList[j].image_url;
+        genreList[i].querySelector("img").setAttribute("id",jsonList[j].id );
+    }
+}
+
+function modalData(json) {
+    document.querySelector("[data-img]").innerHTML = json.image_url;
+    document.querySelector("[data-title]").innerHTML = json.title;
+    document.querySelector("[data-genre]").innerHTML = json.genres;
+}
+
+function createModal() {
+    divModal = document.createElement("div");
+    divModal.className = "modal";
+    divModal.setAttribute("id", "");
+
+    divDialog = document.createElement("div");
+    divDialog.className = "modal-dialog";
+
+    divContent = document.createElement("div");
+    divContent.className = "modal-content";
+    divDialog.appendChild(divContent);
+
+    span = document.createElement("span");
+    span.className = "close";
+    span.setAttribute("data-close", "");
+
+    dataImg = document.createElement("p");
+    dataImg.setAttribute("data-img", "");
+    
+    dataTitle = document.createElement("p");
+    dataTitle.setAttribute("data-title", "");
+
+    dataGenre = document.createElement("p");
+    dataGenre.setAttribute("data-genre", "");
+
+    divContent.append
+    
+    (span, dataImg, dataTitle, dataGenre);
+
+    divModal.appendChild(divDialog);
+    return divModal;
+}
+
+
+async function main() {
+    await getMoviesList(allUrls[0], bestRating);
+    await getMoviesList(allUrls[1], fantasy);
+    await getMoviesList(allUrls[2], comedy);
+    await getMoviesList(allUrls[3], drama);
+
+    bestFilm(bestRating[0], movieGenres[0]);
+    carousel(bestRating, movieGenres[1]);
+    carousel(fantasy, movieGenres[2]);
+    carousel(comedy, movieGenres[3]);
+    carousel(drama, movieGenres[4]);
+}
+
+main();
 
 var btnOpen = document.querySelector("[data-open]");
 var btnClose = document.querySelector(".close");
@@ -73,20 +137,3 @@ btnClose.addEventListener("click", function() {
     document.getElementById(modalId).classList.remove(isVisible);
     document.querySelector("body").style.overflow = "visible";
 });
-
-// var modal = document.querySelector(".modal");
-// var btnOpen = document.querySelector("#open-modal");
-// var btnClose = document.querySelector(".close");
-
-// btnOpen.onclick = function() {
-//     modal.style.visibility = "visible";
-// }
-
-// btnClose.onclick = function() {
-//     modal.style.visibility = "hidden";
-// }
-
-
-
-    
-    
