7f238e78511ab7df54797be5c1e9cbd7666c80c3
Aura <75125762+Aura974@users.noreply.github.com>
2022-03-09 14:23:19 +0400

    Correction bugs mineurs, ajout des doc strings et fichier README

---
+++ README.md
@@ -0,0 +1,39 @@
+
+# ***JustStreamIt***
+
+## Description
+
+L'application JustStreamIt est une application web permettant d'obtenir des informations sur les films les mieux notés.
+En parcourant une base de données (API), elle affiche les films les mieux notés selon une catégorie donnée.  
+
+&nbsp;  
+
+## Configuration requise
+  
+Un navigateur Internet (*exemple* : [Chrome](https://www.google.com/chrome/)).  
+Une connexion Internet (pour interroger l'API).
+
+&nbsp;
+
+## Installation et configuration
+
+1. **Clôner le dépôt de l'application**  
+
+2. **Installer l'API**
+
+    * Télécharger le code de l'API à cet endroit : *<https://github.com/OpenClassrooms-Student-Center/OCMovies-API-EN-FR>*  
+    Il est possible de clôner le dépôt ou de télécharger le dossier ZIP.
+
+    &nbsp;  
+    * Placer les fichiers dans votre application. Il est possible de renommer le dossier de l'API.
+
+    &nbsp;
+    * Suivre les instructions pour la création de l'environnement virtuel, l'installation des dépendances et le démarrage de l'API.  
+    &nbsp;  
+
+3. **Lancer l'application**
+
+    * Une fois l'étape 2 terminée, ouvrir le fichier HTML *JustStreeamIt* dans un navigateur web.  
+
+    &nbsp;
+    * Ne pas oublier de lancer l'API avant chaque démarrage de l'application.
--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const baseUrl = "http://localhost:8000/api/v1/titles/";
-const allUrls = [
+const BASE_URL = "http://localhost:8000/api/v1/titles/";
+const ALL_URLS = [
     "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7",
     "http://localhost:8000/api/v1/titles/?genre=fantasy&sort_by=-imdb_score&page_size=7",
     "http://localhost:8000/api/v1/titles/?genre=comedy&sort_by=-imdb_score&page_size=7",
     "http://localhost:8000/api/v1/titles/?genre=drama&sort_by=-imdb_score&page_size=7"];
-const movieGenres = [
+const MOVIE_GENRES = [
     document.getElementById("best-film"),
     document.querySelectorAll("#carousel1 img"),
     document.querySelectorAll("#carousel2 img"),
     document.querySelectorAll("#carousel3 img"),
-    document.querySelectorAll("#carousel4 img")]
+    document.querySelectorAll("#carousel4 img")];
 
 var bestRating = [];
 var fantasy = [];
@@ -17,123 +17,146 @@ var comedy = [];
 var drama = [];
 
 class Carousel {
+    /**
+     * @param {HTMLElement} element
+     * @param {Object} options
+     * @param {Object} options.slidesToScroll
+     * @param {Object} options.slidesVisible
+     * @param {Object} options.title
+     */
+
     constructor(element, options={}) {
     
-        this.element = element
+        this.element = element;
         this.options = Object.assign({}, {
             slidesToScroll: 3,
             slidesVisible: 4,
             title: "", 
-        }, options)
-        let children = [].slice.call(element.children)
-        this.currentItem = 0
+        }, options);
+        let children = [].slice.call(element.children); // Only access the element children
+        this.currentItem = 0;
 
-        this.title = document.createElement("h1")
-        this.title.setAttribute("class", "title")
-        this.title.innerHTML = this.options.title
-        this.element.appendChild(this.title)
+        // Create the carousel title
+        this.title = document.createElement("h1");
+        this.title.setAttribute("class", "title");
+        this.title.innerHTML = this.options.title;
+        this.element.appendChild(this.title);
     
-        this.root = this.createDivWithClass("carousel")
-        this.container = this.createDivWithClass("carousel__container")
-        this.root.appendChild(this.container)
-        this.element.appendChild(this.root)
+        // Create the carousel outlay
+        this.root = this.createDivWithClass("carousel");
+        this.container = this.createDivWithClass("carousel__container");
+        this.root.appendChild(this.container);
+        this.element.appendChild(this.root);
 
-    
+        // Transform the DOM elements into a carousel
         this.items = children.map((child) => {
-          let item = this.createDivWithClass("carousel__item")
-          item.appendChild(child)
-          this.container.appendChild(item)
-          return item
-        })
+          let item = this.createDivWithClass("carousel__item");
+          item.appendChild(child);
+          this.container.appendChild(item);
+          return item;
+        });
 
     
-        this.setStyle()
-        this.createNavigation()
+        this.setStyle();
+        this.createNavigation();
     
     }
     
     createDivWithClass (className) {
-    let div = document.createElement("div")
-    div.setAttribute("class", className)
-    return div
+    let div = document.createElement("div");
+    div.setAttribute("class", className);
+    return div;
     }
 
+    
+    // Set the correct size to the visible carousel and items
     setStyle () {
-    let ratio = this.items.length / this.options.slidesVisible
-    this.container.style.width = (ratio * 100) + "%"
-    this.items.forEach(item => item.style.width = ((100 / this.options.slidesVisible / ratio) + "%"))
+    let ratio = this.items.length / this.options.slidesVisible;
+    this.container.style.width = (ratio * 100) + "%";
+    this.items.forEach(item => item.style.width = ((100 / this.options.slidesVisible / ratio) + "%"));
     }
 
+    
+    // Create the next and previous buttons
     createNavigation() {
-    let nextButton = this.createDivWithClass("carousel__next")
-    nextButton.innerHTML = "&rsaquo;"
-    let prevButton = this.createDivWithClass("carousel__prev")
-    prevButton.innerHTML = "&lsaquo;"
-    this.root.appendChild(nextButton)
-    this.root.appendChild(prevButton)
-    nextButton.addEventListener("click", this.next.bind(this))
-    prevButton.addEventListener("click", this.prev.bind(this))
+    let nextButton = this.createDivWithClass("carousel__next");
+    nextButton.innerHTML = "&rsaquo;";
+    let prevButton = this.createDivWithClass("carousel__prev");
+    prevButton.innerHTML = "&lsaquo;";
+    this.root.appendChild(nextButton);
+    this.root.appendChild(prevButton);
+    nextButton.addEventListener("click", this.next.bind(this));
+    prevButton.addEventListener("click", this.prev.bind(this));
     }
 
     next() {
-    this.goToItem(this.currentItem + this.options.slidesToScroll)
+    this.goToItem(this.currentItem + this.options.slidesToScroll);
     }
 
     prev() {
-    this.goToItem(this.currentItem - this.options.slidesToScroll)
+    this.goToItem(this.currentItem - this.options.slidesToScroll);
     }
 
+    // Move the carousel to the wanted element (index)
     goToItem(index) {
     if (index < 0) {
-        index = this.items.length - this.options.slidesVisible
+        index = this.items.length - this.options.slidesVisible;
     } else if (index >= this.items.length || this.items[this.currentItem + this.options.slidesVisible] === undefined) {
-        index = 0
+        index = 0;
     }
-    let translateX = index * -100 / this.items.length
-    this.container.style.transform = "translate3d(" + translateX + "%, 0, 0)"
-    this.currentItem = index
-    }
-        
+    let translateX = index * -100 / this.items.length;
+    this.container.style.transform = "translate3d(" + translateX + "%, 0, 0)";
+    this.currentItem = index;
+    }   
 }
 
 class Modal {
+    /**
+     * @param {HTMLElement} element
+     * @param {Object} options
+     * @param {Object} options.id 
+     */
+
     constructor(element, json, options={}) {
     
-        this.element = element
-        this.json = json
+        this.element = element;
+        this.json = json;
         this.options = Object.assign({}, {
             id: 1, 
-        }, options)
+        }, options);
     
-        this.modalRoot = document.createElement("aside")
-        this.modalContainer = this.createDivWithClass("modal")
-        this.modalContainer.setAttribute("id", this.options.id)
-        this.modalDialog = this.createDivWithClass("modal__dialog")
-        this.modalContent = this.createDivWithClass("modal__content")
-        this.modalDialog.appendChild(this.modalContent)
-        this.modalContainer.appendChild(this.modalDialog)
-        this.modalRoot.appendChild(this.modalContainer)
-        this.wrapper = document.querySelector("#wrapper")
-        this.wrapper.appendChild(this.modalRoot)
+        // Create the modal HTML in the DOM
+        this.modalRoot = document.createElement("aside");
+        this.modalContainer = this.createDivWithClass("modal");
+        this.modalContainer.setAttribute("id", this.options.id);
+        this.modalDialog = this.createDivWithClass("modal__dialog");
+        this.modalContent = this.createDivWithClass("modal__content");
+        this.modalDialog.appendChild(this.modalContent);
+        this.modalContainer.appendChild(this.modalDialog);
+        this.modalRoot.appendChild(this.modalContainer);
+        this.wrapper = document.querySelector("#wrapper");
+        this.wrapper.appendChild(this.modalRoot);
 
-        this.span = document.createElement("span")
-        this.span.setAttribute("class", "close")
-        this.span.innerHTML = "x"
-        this.image = document.createElement("img")
-        this.image.setAttribute("class", "modal__img")
-        this.image.src = this.json.image_url
-        this.parag = document.createElement("p")
-        this.parag.setAttribute("class", "data-info")
-        this.modalContent.append(this.span, this.image, this.parag)
+        // Create and add the modal content to the DOM
+        this.span = document.createElement("span");
+        this.span.setAttribute("class", "close");
+        this.span.innerHTML = "x";
+        this.image = document.createElement("img");
+        this.image.setAttribute("class", "modal__img");
+        this.image.src = this.json.image_url;
+        this.parag = document.createElement("p");
+        this.parag.setAttribute("class", "modal__info");
+        this.modalContent.append(this.span, this.image, this.parag);
         
-        let genres = this.addSpaces(json.genres)
-        let actors = this.addSpaces(json.actors)
-        let directors = this.addSpaces(json.directors)
-        let d = new Date(json.date_published)
-        let rated = this.checkIfNull(json.rated, "Not rated or unkown rating")
-        let income = this.checkIfNull(json.worldwide_gross_income, null)
-        let hours = Math.floor(json.duration / 60)
-        let minutes = Math.round(json.duration % 60)
+        // Modify the information inside the modal
+        let genres = this.addSpaces(json.genres);
+        let actors = this.addSpaces(json.actors);
+        let directors = this.addSpaces(json.directors);
+        let d = new Date(json.date_published);
+        let rated = this.checkIfNull(json.rated, "Not rated or unkown rating");
+        let income = this.checkIfNull(json.worldwide_gross_income, null);
+        let hours = Math.floor(json.duration / 60);
+        let minutes = Math.round(json.duration % 60);
 
         this.parag.innerHTML = 
         "<strong>Titre : </strong>" + json.title 
@@ -146,9 +169,10 @@ class Modal {
         +"<br><br><strong>Durée : </strong>" + hours + "h " + minutes + "min"
         +"<br><br><strong>Pays d'origine : </strong>" + json.countries
         +"<br><br><strong>Box-Office : </strong>" + income
-        +"<br><br><strong>Synopsis : </strong>" + json.description
+        +"<br><br><strong>Synopsis : </strong>" + json.description;
     }
 
+    // Add spaces to the lists of strings
     addSpaces(entry) {
         let list = "";
         for (let i in entry) {
@@ -162,7 +186,7 @@ class Modal {
     }
 
     checkIfNull(entry, text) {
-        let list = ""
+        let list = "";
         if (entry == text) {
             list = "N/A";
         } else {
@@ -172,25 +196,27 @@ class Modal {
     }
 
     createDivWithClass (className) {
-        let div = document.createElement("div")
-        div.setAttribute("class", className)
-        return div
+        let div = document.createElement("div");
+        div.setAttribute("class", className);
+        return div;
     }
 }
 
 
+// Get the movies urls from the genre urls
 async function getMoviesList(url, movieData) {
-    response = await fetch(url);
-    resJson = await response.json();
+    let response = await fetch(url);
+    let resJson = await response.json();
     for (var i = 0; i < resJson.results.length; i++) {
-        movieData.push(baseUrl + resJson.results[i].id);
+        movieData.push(BASE_URL + resJson.results[i].id);
     }
 }
 
 
+// Add data to the best film part and create its modal
 async function bestFilm(url, genre) {
-    response = await fetch(url);
-    resJson = await response.json();
+    let response = await fetch(url);
+    let resJson = await response.json();
 
     genre.querySelector("#best-film__img").src = resJson.image_url;
     genre.querySelector("#best-film__title").innerText = resJson.title;
@@ -198,103 +224,105 @@ async function bestFilm(url, genre) {
 
     new Modal (genre, resJson, {
         id: resJson.id
-    })
+    });
 
     genre.querySelector("#open-modal").dataset.open = resJson.id;
 }
 
 
-async function carouselImages(urlList, genreList) {
-    const jsonList = [];
+// Add data to each item in the carousels and create their modals
+async function carouselData(urlList, genreList) {
+    const JSON_LIST = [];
 
-    for (const url of urlList){
-        response = await fetch(url);
-        resJson = await response.json();
-        jsonList.push(resJson);
+    for (const URL of urlList){
+        let response = await fetch(URL);
+        let resJson = await response.json();
+        JSON_LIST.push(resJson);
     }
     
     for (var i = 0; i < genreList.length; i++) {
-        genreList[i].src = jsonList[i].image_url;
-        genreList[i].setAttribute("data-open", jsonList[i].id);
-        new Modal (genreList[i], jsonList[i], {
-            id: jsonList[i].id
-        })
+        genreList[i].src = JSON_LIST[i].image_url;
+        genreList[i].setAttribute("data-open", JSON_LIST[i].id);
+        new Modal (genreList[i], JSON_LIST[i], {
+            id: JSON_LIST[i].id
+        });
     }
 
 }
 
 
+// Call all functions to create the json lists
+async function main() {
+    await getMoviesList(ALL_URLS[0], bestRating);
+    await getMoviesList(ALL_URLS[1], fantasy);
+    await getMoviesList(ALL_URLS[2], comedy);
+    await getMoviesList(ALL_URLS[3], drama);
+
+    bestFilm(bestRating[0], MOVIE_GENRES[0]);
+    carouselData(bestRating, MOVIE_GENRES[1]);
+    carouselData(fantasy, MOVIE_GENRES[2]);
+    carouselData(comedy, MOVIE_GENRES[3]);
+    carouselData(drama, MOVIE_GENRES[4]);
+}
 
 
-
+// Wait for DOM to load because of the asynchronous script
 document.addEventListener("DOMContentLoaded", function() {
 
     new Carousel (document.querySelector("#carousel1"), {
-        title: "Test titre"
-    })
+        title: "Films les mieux notés"
+    });
   
     new Carousel (document.querySelector("#carousel2"), {
-        title: "Test titre 2"
-    })
+        title: "Fantasy"
+    });
 
     new Carousel (document.querySelector("#carousel3"), {
-        title: "Test titre 3"
-    })
+        title: "Comédie"
+    });
 
     new Carousel (document.querySelector("#carousel4"), {
-        title: "Test titre 4"
-    })
+        title: "Drame"
+    });
 
-    var images = document.querySelectorAll(".carousel__item img")
+    var btnOpen = document.querySelector("#open-modal");
+    var isVisible = "is-visible";
+    var images = document.querySelectorAll(".carousel__item img");
 
+    // Opens the best film modal
+    btnOpen.addEventListener("click", function() {
+        var modalId = this.dataset.open;
+        document.getElementById(modalId).classList.add(isVisible);
+        document.querySelector("body").style.overflow = "hidden";
+    });
+
+    // Opens all the matching modals by clicking on the image
     for(var img of images) {
         img.addEventListener("click", function() {
             var modalId = this.dataset.open;
-            document.getElementById(modalId).classList.add(isVisible)
-            document.querySelector("body").style.overflow = "hidden"
-        })
+            document.getElementById(modalId).classList.add(isVisible);
+            document.querySelector("body").style.overflowY = "hidden";
+        });
     }
 
+    // Closes the modal by clicking on the close button
     document.addEventListener("click", function(e) {
         if (e.target && e.target.className == "close") {
             document.querySelector(".modal.is-visible").classList.remove(isVisible);
-            document.querySelector("body").style.overflow = "visible";
+            document.querySelector("body").style.overflowY = "visible";
         }
-    })
+    });
     
+    // Closes the modal by clicking outside of the window
     document.addEventListener("click", function(e) {
         if (e.target == document.querySelector(".modal.is-visible")) {
             document.querySelector(".modal.is-visible").classList.remove(isVisible);
-            document.querySelector("body").style.overflow = "visible";
+            document.querySelector("body").style.overflowY = "visible";
         }
     });
   
-})
+});
 
-
-
-async function main() {
-    await getMoviesList(allUrls[0], bestRating);
-    await getMoviesList(allUrls[1], fantasy);
-    await getMoviesList(allUrls[2], comedy);
-    await getMoviesList(allUrls[3], drama);
-
-    bestFilm(bestRating[0], movieGenres[0]);
-    carouselImages(bestRating, movieGenres[1]);
-    carouselImages(fantasy, movieGenres[2]);
-    carouselImages(comedy, movieGenres[3]);
-    carouselImages(drama, movieGenres[4]);
-}
 
 main();
 
-var btnOpen = document.querySelector("#open-modal");
-
-
-var isVisible = "is-visible";
-
-btnOpen.addEventListener("click", function() {
-    var modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isVisible);
-    document.querySelector("body").style.overflow = "hidden";
-});
